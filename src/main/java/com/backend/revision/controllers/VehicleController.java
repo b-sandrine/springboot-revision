16a60c64ad4ba3ff3659ace45be8a65f9ae15052
b-sandrine <sanrdushimimana@gmail.com>
2023-06-14 23:01:36 +0200

    Basic structure

---
+++ src/main/java/com/backend/revision/controllers/VehicleController.java
@@ -0,0 +1,10 @@
+package com.backend.revision.controllers;
+
+public class VehicleController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/backend/revision/services/VehicleService.java
@@ -0,0 +1,10 @@
+package com.backend.revision.services;
+
+public class VehicleService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
