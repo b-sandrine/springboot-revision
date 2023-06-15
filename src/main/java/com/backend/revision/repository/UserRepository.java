bbfc4cd2f7d1859f017c72a05e8d5c808b55dd4d
b-sandrine <sanrdushimimana@gmail.com>
2023-06-15 09:25:47 +0200

    Added find by email

--- src/main/java/com/backend/revision/repository/UserRepository.java
+++ src/main/java/com/backend/revision/repository/UserRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, List>{
-	
+	List<User> findByEmail(String email);
 }
