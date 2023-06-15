208163f3971e586dbf5ec8a731fd97933f84a7cd
b-sandrine <sanrdushimimana@gmail.com>
2023-06-15 09:38:09 +0200

    Created api for getting user

--- src/main/java/com/backend/revision/controllers/UserController.java
+++ src/main/java/com/backend/revision/controllers/UserController.java
@@ -1,5 +1,25 @@
 package com.backend.revision.controllers;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.revision.models.*;
+import com.backend.revision.repository.*;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
 }
